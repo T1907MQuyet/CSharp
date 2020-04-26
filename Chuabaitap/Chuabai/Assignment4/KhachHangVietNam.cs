615bf1a5fcee7338ea22041cea8c3e8611bf1023
quyet-tc <53265406+quyet-tc@users.noreply.github.com>
2020-04-26 16:33:56 +0700

    up code baitap

---
+++ Chuabaitap/Chuabai/Assignment4/KhachHangVietNam.cs
@@ -0,0 +1,21 @@
+﻿using Chuabai.Assignment4;
+
+namespace Chuabai.Session5
+{
+    public class KhachHangVietNam:KhachHang
+    {
+        private string doituong;
+
+        public KhachHangVietNam(int id, string name, string billDate, int number, string doituong) : base(id, name, billDate, number)
+        {
+            this.doituong = doituong;
+        }
+
+        public string Doituong
+        {
+            get => doituong;
+            set => doituong = value;
+        }
+
+    }
+}
---
+++ Chuabaitap/Chuabai/Session5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Chuabai.Session5
+{
+    public class Program
+    {
+        public static int ProgramCode;
+    }
+
+    namespace MyNamespace
+    {
+        public class MyClass
+        {
+            public void ShowInfo()
+            {
+                 
+            }
+        }
+    }
+}
+// tạo đối tượng Program của namespace Consoleapp 1 
+
