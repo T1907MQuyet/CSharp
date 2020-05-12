004e92268bcc0dce0b48d475a213fa7a8e2325f4
quyet-tc <53265406+quyet-tc@users.noreply.github.com>
2020-05-12 10:46:08 +0700

    Practical

---
+++ Chuabaitap/Chuabai/Session8/Session8_2.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace Chuabai.Session8
+{
+    public class Session8_2
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static void RunCounter(object o)
+        {
+            NumberCounter nc = (NumberCounter) 0;
+            for (int i = 0; i < 100; i++)
+            {
+                lock (nc)
+                {
+                    nc.Change();
+                    nc.Show();
+                }
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
